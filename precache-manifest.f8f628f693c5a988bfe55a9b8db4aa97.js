a79d0265b199faa8ad5e651d386f55368efb51d3
unknown <7Lazev@gmail.com>
2020-02-04 21:15:47 +0300

    Updates

---
+++ precache-manifest.f8f628f693c5a988bfe55a9b8db4aa97.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdee876e919cc6626c1268843e9159b",
+    "url": "/final_project/index.html"
+  },
+  {
+    "revision": "2051f69bc9cc17795f20",
+    "url": "/final_project/static/css/main.d3e9f72c.chunk.css"
+  },
+  {
+    "revision": "3f64b76119502e559811",
+    "url": "/final_project/static/js/2.a88e8c5e.chunk.js"
+  },
+  {
+    "revision": "2051f69bc9cc17795f20",
+    "url": "/final_project/static/js/main.95ba20ca.chunk.js"
+  },
+  {
+    "revision": "323b3b7c177ab2011dd1",
+    "url": "/final_project/static/js/runtime-main.104b370f.js"
+  },
+  {
+    "revision": "e2661c13464cacbc2c363875ae944bc0",
+    "url": "/final_project/static/media/1.e2661c13.png"
+  },
+  {
+    "revision": "5c4812522735590e0a332b13f944f29e",
+    "url": "/final_project/static/media/2.5c481252.png"
+  },
+  {
+    "revision": "b899110cec6aea2bffc0a8e3b57bc9d7",
+    "url": "/final_project/static/media/3.b899110c.png"
+  },
+  {
+    "revision": "82a3a213989980098f01703e9d8d1e9c",
+    "url": "/final_project/static/media/4.82a3a213.png"
+  },
+  {
+    "revision": "11a189ea80822b3f629ce0af241fca03",
+    "url": "/final_project/static/media/5.11a189ea.png"
+  },
+  {
+    "revision": "790e8902417b1012a25f1a8a1721927b",
+    "url": "/final_project/static/media/Logo.790e8902.png"
+  },
+  {
+    "revision": "c7903a64bf4ff258ab7d08aa7047087a",
+    "url": "/final_project/static/media/back.c7903a64.png"
+  },
+  {
+    "revision": "c94179378df00aabc87605f2d37e73b0",
+    "url": "/final_project/static/media/background.c9417937.jpg"
+  },
+  {
+    "revision": "142869f951a07132ea252baa081c5dcf",
+    "url": "/final_project/static/media/backgroundAbout.142869f9.jpg"
+  },
+  {
+    "revision": "0f8e789e159dcc08df61455201923173",
+    "url": "/final_project/static/media/default-user.0f8e789e.jpg"
+  }
+]);
